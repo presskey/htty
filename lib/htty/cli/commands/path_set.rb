dc372fb040351f02f32f5ccb2b0ca6c185649820
Alex Gusev <hello.from.code@gmail.com>
2015-10-15 13:48:55 +0300

    Don't URL-encode slashes in 'cd' command

--- lib/htty/cli/commands/path_set.rb
+++ lib/htty/cli/commands/path_set.rb
@@ -40,7 +40,9 @@ class HTTY::CLI::Commands::PathSet < HTTY::CLI::Command
   def perform
     add_request_if_new do |request|
       self.class.notify_if_cookies_cleared request do
-        request.path_set(*escape_or_warn_of_escape_sequences(arguments))
+        request.path_set *(arguments.map do |argument|
+          escape_or_warn_of_escape_sequences(argument.split('/')).join('/')
+        end)
       end
     end
   end
